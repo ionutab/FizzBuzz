dd4aa581da7dea8f018a8997110fdc4562563d46
Alexandru Ionut Bujdei <ext-alexandru.bujdei@flowbird.group>
2021-03-25 11:08:09 +0200

    Revert "split test case in two for better visibility"
    This reverts commit f79e59f44bf229a7db9543baa0cbe38d3444ced8.

--- src/test/java/com/example/FizzBuzzTest.java
+++ src/test/java/com/example/FizzBuzzTest.java
@@ -39,13 +39,9 @@ public class FizzBuzzTest {
     }
 
     @Test
-    void testFizzBuzzRangeWithCorrectInput() {
+    void testFizzBuzzRange() {
         String nominalOutput = FizzBuzz.solveRange(1, 20);
         assertEquals("1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16 17 fizz 19 buzz", nominalOutput);
-    }
-
-    @Test
-    void testFizzBuzzRangeWithIncorrectInput() {
         String emptyOutput = FizzBuzz.solveRange(20, 19);
         assertEquals("", emptyOutput);
     }
